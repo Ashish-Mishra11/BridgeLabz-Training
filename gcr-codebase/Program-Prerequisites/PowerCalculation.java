1427248f50531e0b1388e1b0748981a5c3494d2e
Ashish-Mishra11 <mishra11122003@gmail.com>
2025-12-17 01:03:11 +0530

    [Ashish Mishra]: Added for Power Calculation

---
+++ gcr-codebase/Program-Prerequisites/PowerCalculation.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class PowerCalculation
+{
+  public static void main(String [] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter base: ");
+    double base=sc.nextDouble();
+    System.out.println("Enter exponent: ");
+    double exponent=sc.nextDouble();
+    double powerCalculation=Math.pow(base,exponent);
+    System.out.println("Result is: "+powerCalculation);
+    sc.close();
+  }
+}

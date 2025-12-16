5a71d8193211447925bfb33baccb94ac6b7c2875
Ashish-Mishra11 <mishra11122003@gmail.com>
2025-12-17 00:58:15 +0530

    [Ashish Mishra]: Added for Calculate Simple Interest

---
+++ gcr-codebase/Program-Prerequisites/SimpleInterest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class SimpleInterest
+{
+  public static void main(String [] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the principal: ");
+    double principal=sc.nextDouble();
+    System.out.println("Enter the rate: ");
+    double rate=sc.nextDouble();
+    System.out.println("Enter the time: ");
+    double time=sc.nextDouble();
+    double simpleInterest=(principal*rate*time)/100;
+    System.out.println("Simple interest for given data: "+simpleInterest);
+    sc.close();
+  }
+}

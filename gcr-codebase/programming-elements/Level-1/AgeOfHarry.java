d215f95cf8fdde12b8b91ef87de71094be228ca3
Ashish-Mishra11 <mishra11122003@gmail.com>
2025-12-17 17:21:31 +0530

    [Ashish Mishra]: Added for Age of Harry

---
+++ gcr-codebase/programming-elements/Level-1/AgeOfHarry.java
@@ -0,0 +1,16 @@
+// Writing a java code to calculate the Harry Age 
+// Create AgeOfHarry class to compute the age of Harry in 2024
+public class AgeOfHarry
+{
+  public static void main(String [] args)
+  {
+    // Create a variable birthYear to store the harry's birth year
+    int birthYear = 2000;
+
+    // Create a variable currentYear to store the current year
+    int currentYear = 2024;
+
+    // Display the Harry's present age 
+    System.out.println("Harry's age in 2024 is: " + (currentYear-birthYear));
+  }
+}

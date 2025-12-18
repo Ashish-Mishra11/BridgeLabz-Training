a70f73eebadc4e83b62cda6ca335b6d25f276b6b
Ashish-Mishra11 <mishra11122003@gmail.com>
2025-12-18 23:25:46 +0530

    [Ashish Mishra]: Added for finding sum until zero or negative number using break

---
+++ gcr-codebase/control-flow/Level-1/SumOfNumbersV2.java
@@ -0,0 +1,28 @@
+// Writing a java code to find the sum of numbers until the user enters 0 or a negative number 
+// Create SumOfNumbersV2 class to find the sum of numbers until the user enters 0 or a negative number 
+
+import java.util.Scanner;
+public class SumOfNumbersV2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        
+    // Input section: Create variable total to store the total answer and intialize it with 0.0
+    double total = 0.0;
+
+    // Logic section: Calculate the sum of given numbers using while loop
+    System.out.println("Enter continuously the number for addition: ");
+    while(true) {
+      System.out.print("Enter the number: ");
+      double userInput = sc.nextDouble();// Create userInput variable to store the value given by user
+      if(userInput <= 0) {
+        break;
+      }
+      total += userInput;// Calculating the total value
+    }
+ 
+    // Display section: Display the total value
+    System.out.println("The total value is: " + total);
+
+    sc.close();
+  }
+}

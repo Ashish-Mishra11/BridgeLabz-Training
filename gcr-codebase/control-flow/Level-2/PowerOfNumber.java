1f0948d562d398e411c27301a3060abd8e41b284
Ashish-Mishra11 <mishra11122003@gmail.com>
2025-12-19 12:44:44 +0530

    [Ashish Mishra]: Added for calculating Power of number using for loop

---
+++ gcr-codebase/control-flow/Level-2/PowerOfNumber.java
@@ -0,0 +1,28 @@
+// Writing a java code to find the power of a number
+// Create PowerOfNumber class to calculate power of a number
+
+import java.util.Scanner;
+public class PowerOfNumber {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    // Input and declaration section: Create variables number and power to store the value given by users 
+    System.out.println("Enter a number: ");
+    int number = sc.nextInt();
+    System.out.println("Enter a power: ");
+    int power = sc.nextInt();
+    int result = 1;// Declare result variable and initialize it with 1
+ 
+    // Logic section: Calculating the power of number by firstly checking the positive number (user input)
+    if(number > 0 && power > 0) {
+      for(int i = 1; i <= power; i++) {
+      result *= number;
+      }
+      System.out.println("The result is: " + result); // Display section: Display the result
+    } else {
+      System.out.println("please enter positive values");
+    }
+    
+    sc.close();
+  }
+}

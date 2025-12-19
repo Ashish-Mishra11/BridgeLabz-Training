8eef7438618be178d02d1d074d2d8da733bd6ba2
Ashish-Mishra11 <mishra11122003@gmail.com>
2025-12-19 16:19:53 +0530

    [Ashish Mishra]: Added for calculation of BMI problem

---
+++ gcr-codebase/control-flow/Level-3/CalculateBMI.java
@@ -0,0 +1,32 @@
+// Writing a java code to find the BMI of a person 
+//create a class CalculateBMI to determine the weight status of the person
+
+import java.util.Scanner;
+public class CalculateBMI {
+  public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+	
+    //Input section: Create weight and height to store value given by user
+    System.out.print("Enter the weight (in KG): ");
+    double weight  = sc.nextDouble();
+    System.out.print("Enter the height (in cm): ");
+    double height= sc.nextDouble();
+		
+    //Logic section: Calculation of BMI
+    double bmi = weight / (height * height / 10000);
+
+    // Display section: Display the results
+    if ( bmi > 40.0) {
+      System.out.println("Obese");
+    } else if ( bmi > 25.0 && bmi < 40.0) {
+      System.out.println("Overweight");
+    } else if ( bmi > 18.5 && bmi < 25) {
+      System.out.println("Normal");
+    } else if ( bmi <= 18.4) {
+      System.out.println("Underweight");
+    }
+
+
+    sc.close();
+  }
+}

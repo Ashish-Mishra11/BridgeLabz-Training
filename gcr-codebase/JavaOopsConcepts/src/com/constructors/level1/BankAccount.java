20eff6ebea7dc63233c56bb4c7786abf91cd9d5a
Ashish-Mishra11 <mishra11122003@gmail.com>
2025-12-28 01:20:11 +0530

    [Ashish Mishra]: Added for bank account super class

---
+++ gcr-codebase/JavaOopsConcepts/src/com/constructors/level1/BankAccount.java
@@ -0,0 +1,23 @@
+package com.constructors.level1;
+
+class BankAccount {
+
+    public int accountNumber;
+    protected String accountHolder;
+    private double balance;
+
+    // Constructor
+    BankAccount(int accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}

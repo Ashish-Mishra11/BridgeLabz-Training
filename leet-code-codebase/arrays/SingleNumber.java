d1f8f83d2987f3e008ea306e409eb83cc94d604e
Ashish-Mishra11 <mishra11122003@gmail.com>
2025-12-28 21:04:52 +0530

    [Ashish Mishra]: Added for single number program

---
+++ leet-code-codebase/arrays/SingleNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class SingleNumber {
+
+    public static int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = singleNumber(nums);
+        System.out.println("Single number is: " + result);
+
+        sc.close();
+    }
+}

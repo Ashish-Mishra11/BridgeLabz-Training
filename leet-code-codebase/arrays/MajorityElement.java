b7257f1a2b286a57933c16d4250ee5367300a5b9
Ashish-Mishra11 <mishra11122003@gmail.com>
2025-12-28 21:06:52 +0530

    [Ashish Mishra]: Added for majority element program

---
+++ leet-code-codebase/arrays/MajorityElement.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class MajorityElement {
+
+    public static int majorityElement(int[] nums) {
+        int count = 1;
+        int candidate = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (candidate == nums[i]) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    candidate = nums[i];
+                    count++;
+                }
+            }
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = majorityElement(nums);
+        System.out.println("Majority element is: " + result);
+
+        sc.close();
+    }
+}

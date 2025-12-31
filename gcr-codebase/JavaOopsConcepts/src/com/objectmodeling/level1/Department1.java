23b2e58c74edd452c33c70b8c0bc1b2e9e1c0a9d
Ashish-Mishra11 <mishra11122003@gmail.com>
2025-12-31 11:42:13 +0530

    [Ashish Mishra]: Added for department 1 program

---
+++ gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/level1/Department1.java
@@ -0,0 +1,10 @@
+package com.objectmodeling.level1;
+
+public class Department1 {
+	String departmentName;
+
+    public Department1(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+}

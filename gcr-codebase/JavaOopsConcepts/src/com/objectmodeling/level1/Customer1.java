b019accf5bac95171499378159785dc4bf192bdc
Ashish-Mishra11 <mishra11122003@gmail.com>
2025-12-31 11:41:17 +0530

    [Ashish Mishra]: Added for customer 1 program

---
+++ gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/level1/Customer1.java
@@ -0,0 +1,15 @@
+package com.objectmodeling.level1;
+
+public class Customer1 {
+	String name;
+
+    public Customer1(String name) {
+        this.name = name;
+    }
+
+    public void placeOrder(Order order) {
+        System.out.println(name + " placed an order:");
+        order.showOrderDetails();
+    }
+
+}

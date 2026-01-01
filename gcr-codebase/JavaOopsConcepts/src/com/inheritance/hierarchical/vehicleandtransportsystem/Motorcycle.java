bee42a9a78b5957dea72646255e6ba6218ae17ed
Ashish-Mishra11 <mishra11122003@gmail.com>
2026-01-01 12:37:46 +0530

    [Ashish Mishra]: Added Motorcycle.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/vehicleandtransportsystem/Motorcycle.java
@@ -0,0 +1,17 @@
+package com.inheritance.hierarchical.vehicleandtransportsystem;
+
+public class Motorcycle extends Vehicle {
+	boolean hasCarrier;
+
+    Motorcycle(int maxSpeed, String fuelType, boolean hasCarrier) {
+        super(maxSpeed, fuelType);
+        this.hasCarrier = hasCarrier;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Carrier: " + hasCarrier);
+    }
+
+}

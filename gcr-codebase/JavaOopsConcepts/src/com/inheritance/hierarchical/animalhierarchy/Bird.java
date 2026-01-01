de431f762a707306dbe647cc61b9e4a51e8a36fc
Ashish-Mishra11 <mishra11122003@gmail.com>
2026-01-01 12:21:14 +0530

    [Ashish Mishra]: Added Bird.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/animalhierarchy/Bird.java
@@ -0,0 +1,13 @@
+package com.inheritance.hierarchical.animalhierarchy;
+
+public class Bird extends Animal {
+	Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("Bird chirps");
+    }
+
+}

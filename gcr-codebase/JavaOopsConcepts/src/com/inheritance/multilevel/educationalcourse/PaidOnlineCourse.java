2fe0eab335ceb8359bd18fd46117af23256d533f
Ashish-Mishra11 <mishra11122003@gmail.com>
2026-01-01 13:19:30 +0530

    [Ashish Mishra]: Added PaidOnlineCourse.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevel/educationalcourse/PaidOnlineCourse.java
@@ -0,0 +1,21 @@
+package com.inheritance.multilevel.educationalcourse;
+
+public class PaidOnlineCourse extends OnlineCourse {
+	private double fee;
+    private double discount;
+
+    PaidOnlineCourse(String courseName, int duration, String platform, boolean isRecorded, double fee, double discount) {
+        super(courseName, duration, platform, isRecorded);
+        this.fee = fee;
+        this.discount = discount;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Fee: " + fee);
+        System.out.println("Discount: " + discount);
+        System.out.println("Final Price: " + (fee - discount));
+    }
+
+}

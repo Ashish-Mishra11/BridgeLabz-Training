cb22d6fac80f2d94157b69e47592326845668eef
Ashish-Mishra11 <mishra11122003@gmail.com>
2026-01-05 01:33:53 +0530

    [Ashish Mishra]: Added program files for movie management system

---
+++ gcr-codebase/DSAConcepts/src/com/linkedlists/doubly/moviemanagementsystem/MovieManagementApp.java
@@ -0,0 +1,96 @@
+package com.linkedlists.doubly.moviemanagementsystem;
+import java.util.Scanner;
+
+public class MovieManagementApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        MovieDoublyLinkedList movies = new MovieDoublyLinkedList();
+        int choice;
+
+        do {
+            System.out.println("\n--- Movie Management System ---");
+            System.out.println("1. Add Movie at Beginning");
+            System.out.println("2. Add Movie at End");
+            System.out.println("3. Add Movie at Position");
+            System.out.println("4. Remove Movie by Title");
+            System.out.println("5. Search by Director");
+            System.out.println("6. Search by Rating");
+            System.out.println("7. Update Rating");
+            System.out.println("8. Display Movies (Forward)");
+            System.out.println("9. Display Movies (Reverse)");
+            System.out.println("0. Exit");
+            System.out.print("Enter choice: ");
+
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                case 2:
+                case 3:
+                    System.out.print("Title: ");
+                    String title = sc.nextLine();
+                    System.out.print("Director: ");
+                    String director = sc.nextLine();
+                    System.out.print("Year: ");
+                    int year = sc.nextInt();
+                    System.out.print("Rating: ");
+                    double rating = sc.nextDouble();
+
+                    if (choice == 1)
+                        movies.addAtBeginning(title, director, year, rating);
+                    else if (choice == 2)
+                        movies.addAtEnd(title, director, year, rating);
+                    else {
+                        System.out.print("Position: ");
+                        int pos = sc.nextInt();
+                        movies.addAtPosition(pos, title, director, year, rating);
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("Enter Movie Title: ");
+                    movies.removeByTitle(sc.nextLine());
+                    break;
+
+                case 5:
+                    System.out.print("Enter Director Name: ");
+                    movies.searchByDirector(sc.nextLine());
+                    break;
+
+                case 6:
+                    System.out.print("Enter Rating: ");
+                    movies.searchByRating(sc.nextDouble());
+                    break;
+
+                case 7:
+                    System.out.print("Movie Title: ");
+                    String t = sc.nextLine();
+                    System.out.print("New Rating: ");
+                    movies.updateRating(t, sc.nextDouble());
+                    break;
+
+                case 8:
+                    movies.displayForward();
+                    break;
+
+                case 9:
+                    movies.displayReverse();
+                    break;
+
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+
+        } while (choice != 0);
+
+        sc.close();
+    }
+}

187acd80ed442e9b12293d2165d7b9117f476e09
Ashish-Mishra11 <mishra11122003@gmail.com>
2026-01-05 14:37:56 +0530

    [Ashish Mishra]: Added program files for zero sum subarrays

---
+++ gcr-codebase/DSAConcepts/src/com/hashmaps/zerosumsubarrays/ZeroSumSubarrays.java
@@ -0,0 +1,32 @@
+package com.hashmaps.zerosumsubarrays;
+import java.util.*;
+
+public class ZeroSumSubarrays {
+
+    public static void findZeroSumSubarrays(int[] arr) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        
+        // To handle subarrays starting from index 0
+        map.put(0, new ArrayList<>(Arrays.asList(-1)));
+        
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (map.containsKey(sum)) {
+                for (int startIndex : map.get(sum)) {
+                    System.out.println("Zero-sum subarray found from index "
+                            + (startIndex + 1) + " to " + i);
+                }
+            }
+
+            map.computeIfAbsent(sum, k -> new ArrayList<>()).add(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, -7, 3, 1, 3, -4, -2, -2};
+        findZeroSumSubarrays(arr);
+    }
+}

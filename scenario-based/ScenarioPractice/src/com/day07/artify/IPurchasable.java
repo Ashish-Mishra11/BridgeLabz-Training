f4fab720b8cb2f16a44997e77da2a0a88de865d0
Ashish-Mishra11 <mishra11122003@gmail.com>
2026-01-10 09:34:02 +0530

    [Ashish Mishra]: Added program files for artify problem

---
+++ scenario-based/ScenarioPractice/src/com/day07/artify/IPurchasable.java
@@ -0,0 +1,7 @@
+package com.day07.artify;
+
+public interface IPurchasable {
+    void purchase();
+    boolean license();
+
+}
---
+++ scenario-based/ScenarioPractice/src/com/day07/artify/PrintArt.java
@@ -0,0 +1,11 @@
+package com.day07.artify;
+
+public class PrintArt extends Artwork {
+	PrintArt(String title, String artist, double price, String licenseType){
+		this.title = title;
+		this.artist = artist;
+		this.price = price;
+		this.licenseType = licenseType;
+	}
+
+}

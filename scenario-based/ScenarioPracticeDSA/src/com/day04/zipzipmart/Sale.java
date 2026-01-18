965db0c6830b7c9d59484acf4e2402b7a3ec5de0
Ashish-Mishra11 <mishra11122003@gmail.com>
2026-01-19 00:50:06 +0530

    [Ashish Mishra]: Added program files for zipzip mart problem

---
+++ scenario-based/ScenarioPracticeDSA/src/com/day04/zipzipmart/Sale.java
@@ -0,0 +1,15 @@
+package com.day04.zipzipmart;
+
+import java.time.LocalDate;
+
+class Sale {
+    LocalDate date;
+    double amount;
+    String branch;
+    
+    public Sale(LocalDate date, double amount, String branch) {
+        this.date = date;
+        this.amount = amount;
+        this.branch = branch;
+    }
+}

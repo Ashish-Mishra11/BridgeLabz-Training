84d853a6098d8e83b0c455e55dc9186f3d8238d1
Ashish-Mishra11 <mishra11122003@gmail.com>
2026-01-27 14:22:05 +0530

    [Ashish Mishra]: Added java file for custom annotation problem

---
+++ gcr-codebase/JavaCollectionsPractice/src/com/annotations/customannotation/CustomAnnotationDriver.java
@@ -0,0 +1,17 @@
+package com.annotations.customannotation;
+import java.lang.reflect.Method;
+
+public class CustomAnnotationDriver {
+
+    public static void main(String[] args) throws Exception {
+
+        TaskManager manager = new TaskManager();
+
+        Method method = manager.getClass().getMethod("completeTask");
+
+        TaskInfo taskInfo = method.getAnnotation(TaskInfo.class);
+
+        System.out.println("Priority: " + taskInfo.priority());
+        System.out.println("Assigned To: " + taskInfo.assignedTo());
+    }
+}
